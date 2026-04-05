4ce5e522a05ec8c71a7e7ab4f867edb3c3c4ee7e
tushar981146 <tusharblackhathacker@gmail.com>
2026-04-05 19:48:28 +0530

    fix axios base

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,8 +6,7 @@ import { useAuthStore } from "../store/useAuthStore";
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api/" : import.meta.env.VITE_PROD_BASE_URL,
-    // baseURL: import.meta.env.MODE = "http://localhost:5001/api/",
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api/" : "https://expences-tracker-backend.onrender.com/api/",
+
     withCredentials: true,
 });
 let refresh = null;
